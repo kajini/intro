cc7b9a8d4ba078016a2b0986b6d1b2ee2c14f3cd
glendalys <glendalysmedina02@gmail.com>
2018-08-16 15:58:23 -0400

    'everything'

---
+++ signupp.php
@@ -0,0 +1,13 @@
+<?php
+include 'dbh.php';
+
+$first = $_POST['first'];
+$last = $_POST['last'];
+$email = $_POST['email'];
+$pwd = $_POST['pwd'];
+
+echo $first."<br";
+echo $last."<br";
+echo $email."<br";
+echo $pwd."<br";
+ ?>
